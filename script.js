d3c1620e4e53a407c0436dad18226b97891f515b
pthanomsing <p.thanomsing@outlook.com>
2022-07-05 11:38:05 +0700

    Create computerPlay for to return random play for computer

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 function getRandom(array) {
     return element = array[Math.floor(Math.random()*array.length)];
 }
+
+function computerPlay() {
+
+    const play = ['rock', 'paper', 'scissor'];
+    return getRandom(play);
+}
