9f11593677e526e42c9f3ebc9d030af32f352177
pthanomsing <p.thanomsing@outlook.com>
2022-07-05 11:58:42 +0700

    Implement play function if the player chose rock

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ function computerPlay() {
     const play = ['rock', 'paper', 'scissor'];
     return getRandom(play);
 }
+
+function play(playerSelection, computerSelection) {
+    if (playerSelection == 'rock') {
+        switch (computerSelection) {
+            case 'rock':
+                result = "You draw!, Both of you chose Rock";
+                break;
+            case 'paper':
+                result = "You lose!, Paper beats Rock"
+                break;
+            case 'scissors' :
+                result = "You win!, Rock beats Scissors"
+                break;
+        }
+
+    }
+    return result;
+}
+
